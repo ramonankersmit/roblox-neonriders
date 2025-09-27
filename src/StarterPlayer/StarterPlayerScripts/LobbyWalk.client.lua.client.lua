50842f5eecb50d7ad6927d52dc3b2d24626f8af6
Ramon Ankersmit <ramon_ankersmit@hotmail.com>
2025-09-27 18:05:04 +0200

    Fix lobby movement axes and camera framing

--- src/StarterPlayer/StarterPlayerScripts/LobbyWalk.client.lua.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/LobbyWalk.client.lua.client.lua
@@ -18,19 +18,40 @@ UIS.InputEnded:Connect(function(i,g) if g then return end; for k,code in pairs(k
 RunService.RenderStepped:Connect(function()
         if not canControlHumanoid() then return end
         local char=plr.Character; local hum=char and char:FindFirstChildOfClass("Humanoid"); if not hum then return end
-        if not workspace.CurrentCamera then return end
+        local cam = workspace.CurrentCamera; if not cam then return end
 
-        local x = 0
-        if down.D then x += 1 end
-        if down.A then x -= 1 end
+        local right = 0
+        if down.D then right += 1 end
+        if down.A then right -= 1 end
 
-        local z = 0
-        if down.S then z += 1 end
-        if down.W then z -= 1 end
+        local forward = 0
+        if down.W then forward += 1 end
+        if down.S then forward -= 1 end
 
-        local move = Vector3.new(x, 0, z)
-        if move.Magnitude > 0 then
-                hum:Move(move.Unit, true)
+        if right ~= 0 or forward ~= 0 then
+                local camCF = cam.CFrame
+                local camForward = camCF.LookVector
+                camForward = Vector3.new(camForward.X, 0, camForward.Z)
+                if camForward.Magnitude < 1e-4 then
+                        camForward = Vector3.new(0, 0, -1)
+                else
+                        camForward = camForward.Unit
+                end
+
+                local camRight = camCF.RightVector
+                camRight = Vector3.new(camRight.X, 0, camRight.Z)
+                if camRight.Magnitude < 1e-4 then
+                        camRight = Vector3.new(camForward.Z, 0, -camForward.X)
+                else
+                        camRight = camRight.Unit
+                end
+
+                local moveDir = camForward * forward + camRight * right
+                if moveDir.Magnitude > 1 then
+                        moveDir = moveDir.Unit
+                end
+
+                hum:Move(moveDir, false)
         else
                 hum:Move(Vector3.zero, false)
         end
