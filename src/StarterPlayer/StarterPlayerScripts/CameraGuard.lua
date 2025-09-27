f6c6402811f952b9aa627c5a9061a0363d42e374
Ramon Ankersmit <ramon_ankersmit@hotmail.com>
2025-09-27 23:55:41 +0200

    Hold camera guard slot through the frame

--- src/StarterPlayer/StarterPlayerScripts/CameraGuard.lua
+++ src/StarterPlayer/StarterPlayerScripts/CameraGuard.lua
@@ -11,16 +11,27 @@ local activeWriter = nil
 local activeReason = nil
 local frameSerial = 0
 local warnedForFrame = {}
+local lastReleasedId = nil
+local lastReleaseFrame = 0
 
 RunService.RenderStepped:Connect(function()
     frameSerial += 1
     activeWriter = nil
     activeReason = nil
     warnedForFrame = {}
+    lastReleasedId = nil
+    lastReleaseFrame = frameSerial
 end)
 
 function CameraGuard:tryAcquire(id, reason)
     assert(id, "CameraGuard: id required")
+    if activeWriter == nil then
+        if lastReleaseFrame == frameSerial and lastReleasedId and lastReleasedId ~= id then
+            -- iemand anders heeft deze frame al geschreven; blokkeer overige schrijvers
+            return false
+        end
+    end
+
     if activeWriter == nil or activeWriter == id then
         activeWriter = id
         activeReason = reason
@@ -42,6 +53,9 @@ end
 
 function CameraGuard:release(id)
     if activeWriter == id then
+        lastReleasedId = id
+        lastReleaseFrame = frameSerial
+        -- we houden de writer "bezet" voor de rest van het frame zodat niemand anders meer schrijft
         activeWriter = nil
         activeReason = nil
         return true
